44d0ad95c9aef7f669642c63adae335e670a4449
Max <max@maxdiamondmix.com>
2018-09-29 15:34:27 -0500

    5.2 partial search-form to make sense of error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
 end
+
+def self.search(search_term)
+  if Rails.env.development?
+    Product.where("name LIKE ?", "%#{search_term}%")
+  else Rails.env.production?
+    Product.where("name ilike ?", "%#{search_term}%")
+  end
+end
+
+# def self.search(search_term)
+#   Product.where("name LIKE ?", "%#{search_term}%")
+# end
