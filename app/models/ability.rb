90bfcf9db6f11c46fd03d6caf94deaa103afc7b8
Max <max@maxdiamondmix.com>
2018-11-08 12:55:01 +0100

    5.10 frontend + backend admin-can-manage-destroy-all set up

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,8 +3,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    can :manage, Order, :user_id => user.id
+    if user.admin?  # additional permissions for administrators
+      can :manage, :all
+      # can :destroy, :all
+    else
+      can :manage, User, id: user.id
+      can :manage, Order, :user_id => user.id
+      cannot :destroy, :all
+    end
   end
 end
 
