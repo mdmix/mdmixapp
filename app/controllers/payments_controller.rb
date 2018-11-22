27ad045d62bab1b5c65b43930b74744c586b407a
Max <max@maxdiamondmix.com>
2018-11-22 02:26:37 +0100

    6.6 Stripe orders + order confirmations set up

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,11 +8,11 @@ class PaymentsController < ApplicationController
     # Create the charge on Stripe's servers - this will charge the user's card
     begin
       charge = Stripe::Charge.create(
-        amount: @product.price, # amount in cents, again
+        amount: @product.price*100, # amount in cents, again
         currency: "usd",
         source: token,
         description: params[:stripeEmail]
-        email: @user.email
+        receipt_email: @user.email
       )
 
       if charge.paid
